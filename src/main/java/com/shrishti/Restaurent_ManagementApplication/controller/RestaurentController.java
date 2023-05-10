3473b3b4e7837c6b116b0afd10d6e2827319aa54
Anuhshka Srivastava <shrishtisrivastava19@gmail.com>
2023-05-10 23:20:39 +0530

    New Restaurent Commit

--- src/main/java/com/shrishti/Restaurent_ManagementApplication/controller/RestaurentController.java
+++ src/main/java/com/shrishti/Restaurent_ManagementApplication/controller/RestaurentController.java
@@ -1,7 +1,61 @@
 package com.shrishti.Restaurent_ManagementApplication.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import com.shrishti.Restaurent_ManagementApplication.model.Management;
+import com.shrishti.Restaurent_ManagementApplication.model.RestaurentData.R_Employee;
+import com.shrishti.Restaurent_ManagementApplication.model.Restaurent;
+import com.shrishti.Restaurent_ManagementApplication.model.RestroSignInput;
+import com.shrishti.Restaurent_ManagementApplication.service.RestaurentService;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 public class RestaurentController {
+
+    @Autowired
+    RestaurentService restaurentService;
+
+    //Create Restaurent or Registration
+    @PostMapping(value = "/restaurent")
+    public String createRestaurent(@Valid @RequestBody RestroSignInput restaurents){
+        return restaurentService.createRestaurent(restaurents);
+    }
+
+    //User Service API - Get All Restaurents For User
+    @GetMapping(value = "/userRestaurents")
+    public List<Restaurent> getAllRestaurent(){
+        return restaurentService.getAllRestaurents();
+    }
+
+    //Self-Service API - Restaurent to view own information
+    @GetMapping(value = "/restaurent")
+    public Restaurent findRestaurentById(@RequestParam Integer resId, @RequestParam String password){
+        return restaurentService.findRestaurentById(resId,password);
+    }
+
+    //Self-Service API - Restaurent to Update its own information
+    @PutMapping(value = "/restaurent")
+    public String updateRestaurent(@RequestBody Restaurent restaurent, @RequestParam Integer resId){
+        return restaurentService.updateRestaurent(restaurent,resId);
+    }
+
+    //Management Service - Create Employee
+    @PostMapping(value = "/employee")
+    public String createEmployee(@RequestBody R_Employee employee){
+        return restaurentService.createEmployee(employee);
+    }
+
+    //Management Service - Create Management HR
+    @PostMapping(value = "/management")
+    public String createManagement(@RequestBody Management manage){
+        return restaurentService.createManagement(manage);
+    }
+
+    //Management Service - Open Admin Portal By Id and Password
+    @GetMapping(value = "/managementData")
+    public Management findAdminByIdAndPassword(@RequestParam Integer hrId, @RequestParam String password){
+        return restaurentService.findAdminByIdAndPassword(hrId,password);
+    }
 }
